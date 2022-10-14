aa0fcd47736822c687a1a4c34c0ffc592aacb7e0
YangWooSong <olivia6844@naver.com>
2022-10-14 15:28:26 +0900

    3D item

---
+++ Cave_XP_Collaboration1/Assets/Scripts/Item3D.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item3D : MonoBehaviour
+{
+    public GameObject objectMesh; //item 본인
+    Image S_image; //슬롯 이미지
+    public Sprite EmptyImage;  //투명 이미지
+    public Sprite ItemImage; // 아이템 이미지
+    public GameObject SlotItem; //아이템별 슬롯 지정
+    void Start()
+    {
+        SlotItem.GetComponent<Button>().interactable = false;   //슬롯 버튼 비활성화
+        S_image = SlotItem.GetComponent<Image>();   //슬롯 이미지
+    }
+    private void OnMouseDown()
+    {
+        //Debug.Log("마우스");
+        SlotItem.GetComponent<Button>().interactable = true;    //슬롯 버튼 활성화
+        S_image.sprite = ItemImage;     //슬롯 이미지 아이템으로 변경
+        Destroy(objectMesh);   
+    }
+}
