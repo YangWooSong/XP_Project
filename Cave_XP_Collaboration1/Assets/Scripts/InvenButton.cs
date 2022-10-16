d9dfc7c24e33d0057a932d24d475c3549ef23e94
rudtnsiasia <leeks1106@naver.com>
2022-10-17 00:44:14 +0900

    gameManager_in_timer

--- Cave_XP_Collaboration1/Assets/Scripts/InvenButton.cs
+++ Cave_XP_Collaboration1/Assets/Scripts/InvenButton.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class InvenButton : MonoBehaviour
 {
     public GameObject inventoryPanel;   //인벤토리 패널
-    bool activeInven = false;       //인벤 패널 활성화 상태
+    bool activeInven;       //인벤 패널 활성화 상태
 
     private void Start()
     {
+        
         inventoryPanel.SetActive(activeInven);      //인벤 비활성화
     }
     void Update()
     {
-
+        
     }
 
     public void getButton()
     {
+        Debug.Log("클릭");
+        Debug.Log(MainGameManagerScr.timeGo);
         activeInven = !activeInven;     //버튼 누를때 마다 활성 or 비활성
         inventoryPanel.SetActive(activeInven);      // bool값 적용
+        MainGameManagerScr.timeGo = !MainGameManagerScr.timeGo;
+        
     }
 }
