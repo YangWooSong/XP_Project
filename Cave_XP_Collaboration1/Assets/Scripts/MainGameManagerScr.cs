c29f94ad5a0b62deb15ad35e24f9f41231a6b824
YangWooSong <olivia6844@naver.com>
2022-11-11 20:50:49 +0900

    Inven
    인벤

--- Cave_XP_Collaboration1/Assets/Scripts/MainGameManagerScr.cs
+++ Cave_XP_Collaboration1/Assets/Scripts/MainGameManagerScr.cs
@@ -8,12 +8,16 @@ public class MainGameManagerScr : MonoBehaviour
     public static float timeLimit= 100f;//총 게임 시간 
     public static bool wireClear = false;
     public static bool timeGo = true;
+
+
+    public GameObject inventoryPanel;   //인벤토리 패널
+    bool activeInven;       //인벤 패널 활성화 상태
   
-    // Start is called before the first frame update
 
     void Start()
     {
         instance = this;
+        inventoryPanel.SetActive(activeInven);      //인벤 비활성화
     }
 
     // Update is called once per frame
@@ -40,5 +44,18 @@ public class MainGameManagerScr : MonoBehaviour
     {
         timeLimit += getTime;
         MemoryTimer.MemorytimeDuration = 0;
+    }
+    public void getInvenButton()
+    {
+        Debug.Log("클릭");
+        Debug.Log(MainGameManagerScr.timeGo);
+        activeInven = !activeInven;     //버튼 누를때 마다 활성 or 비활성
+        if(activeInven == true)
+        {
+            
+        }
+        inventoryPanel.SetActive(activeInven);      // bool값 적용
+        MainGameManagerScr.timeGo = !MainGameManagerScr.timeGo;
+
     }
 }
