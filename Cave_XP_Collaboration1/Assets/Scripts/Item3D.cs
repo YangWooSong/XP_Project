c03922b9ba7f6b9cb37646c2bd314812e6004e2e
YangWooSong <olivia6844@naver.com>
2022-11-15 00:19:11 +0900

    Editor
    빌드땜시 오류 고침

--- Cave_XP_Collaboration1/Assets/Scripts/Item3D.cs
+++ Cave_XP_Collaboration1/Assets/Scripts/Item3D.cs
@@ -8,15 +8,15 @@ using UnityEngine.EventSystems;
 public class Item3D : MonoBehaviour
 {
   
-      Image S_image; //슬롯 이미지
-      public Sprite EmptyImage;  //투명 이미지
-      public Sprite ItemImage; // 아이템 이미지
-      public GameObject SlotItem; //아이템별 슬롯 지정
+      //Image S_image; //슬롯 이미지
+      //public Sprite EmptyImage;  //투명 이미지
+     // public Sprite ItemImage; // 아이템 이미지
+     // public GameObject SlotItem; //아이템별 슬롯 지정
    
     void Start()
     {
-        SlotItem.GetComponent<Button>().interactable = false;   //슬롯 버튼 비활성화
-        S_image = SlotItem.GetComponent<Image>();   //슬롯 이미지
+       // SlotItem.GetComponent<Button>().interactable = false;   //슬롯 버튼 비활성화
+        //S_image = SlotItem.GetComponent<Image>();   //슬롯 이미지
 
     }
 
@@ -25,7 +25,7 @@ public class Item3D : MonoBehaviour
     private void OnMouseDown()
     {
         //ui뒤에 물체는 눌리지 않게 하는 코드
-        if (!EventSystem.current.IsPointerOverGameObject())
+       /* if (!EventSystem.current.IsPointerOverGameObject())
         {
             SlotItem.GetComponent<Button>().interactable = true;    //슬롯 버튼 활성화
             S_image.sprite = ItemImage;     //슬롯 이미지 아이템으로 변경
@@ -35,7 +35,7 @@ public class Item3D : MonoBehaviour
             }  
             Destroy(gameObject);
 
-        }
+        }*/
 
 
     }
